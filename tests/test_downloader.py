89ebd3255b526701cbd6527f6370c0725962ab4f
Yotsuba <happy819tw@gmail.com>
2021-05-18 15:30:36 +0800

    Change importing namespace in test

--- tests/test_downloader.py
+++ tests/test_downloader.py
@@ -1,7 +1,7 @@
 import pytest
 import requests
 import responses
-from jabledl import Downloader
+from jabledl import downloader
 
 
 def callback():
@@ -10,6 +10,6 @@ def callback():
 
 @responses.activate
 def test_download():
-    downloader = Downloader(['http://example.com'] * 200, {}, callback)
+    download = downloader.Downloader(['http://example.com'] * 200, {}, callback)
     responses.add(responses.GET, 'http://example.com', status = 200)
-    downloader.download()
+    download.download()
